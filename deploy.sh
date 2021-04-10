6ba178f53e85255068629cf5ab2818b164d2c659
Kn99HN <Khanhnguyen99hn@gmail.com>
2021-04-10 21:57:29 +0000

    Light mod

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,4 @@
 # Basic script for deploying on DO. Not recommended to use pm2 with typescript
 # but my usage is small enough that this should be fine for NOW!
 cd ~/$1/backend && pm2 reload app.ts --watch
-cd ~/$1/frontend && yarn build
-sudo cp -r build/* /var/www/$2/html
-sudo systemctl restart nginx
+cd ~/$1/frontend && yarn build && sudo cp -r build/* /var/www/$2/html && sudo systemctl restart nginx
